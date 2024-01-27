39ec36cfc2862ca3ea87159a736ef10d29cbac91
GrischK <grischka.gorski@laposte.net>
2024-01-27 17:33:04 +0100

    💄 Change icons, colors for podium

--- client/src/screens/NewRanking/NewRanking.tsx
+++ client/src/screens/NewRanking/NewRanking.tsx
@@ -1,11 +1,13 @@
 import styles from './NewRanding.module.css';
 import React, {useEffect, useState} from "react";
-import MysteriousText from "../../components/MysteriousText";
 import ConfettiExplosion from 'react-confetti-explosion';
 import cheer from '../../assets/sounds/cheer.mp3'
 import trumpets from '../../assets/sounds/fanfare_trumpets.mp3'
 import {useGamesQuery, User} from "../../gql/generated/schema";
 import defaultAvatar from "../../assets/images/default_avatar.png";
+import thirdMedal from "../../assets/images/medal_3.png"
+import secondMedal from "../../assets/images/medal_2.png"
+import cup from "../../assets/images/cup.png"
 
 interface PlayersPoints {
     player: User;
@@ -35,6 +37,8 @@ export default function NewRanking() {
 
     const {data, refetch} = useGamesQuery()
     const [playersPoints, setPlayersPoints] = useState<PlayersPoints[]>([])
+
+    const [step, setStep] = useState(1)
 
     useEffect(() => {
         if (data) {
@@ -66,110 +70,151 @@ export default function NewRanking() {
     }, [data]);
 
     useEffect(() => {
-        const timers: any[] = [];
+        if (step === 2) {
+            const timers: any[] = [];
 
-        const thirdPlayerTimer = setTimeout(() => {
-            setThirdPlayerIsShown(true)
-        }, 1000);
-        const thirdPlayerNameTimer = setTimeout(() => {
-            setThirdPlayerNameIsShown(true)
-        }, 3000);
-        const moveThirdPlayerTimer = setTimeout(() => {
-            setMoveThirdPlayer(true)
-        }, 5000);
+            const thirdPlayerTimer = setTimeout(() => {
+                setThirdPlayerIsShown(true)
+            }, 1000);
+            const thirdPlayerNameTimer = setTimeout(() => {
+                setThirdPlayerNameIsShown(true)
+            }, 3000);
+            const moveThirdPlayerTimer = setTimeout(() => {
+                setMoveThirdPlayer(true)
+            }, 5000);
 
-        timers.push(thirdPlayerTimer, moveThirdPlayerTimer, thirdPlayerNameTimer);
+            timers.push(thirdPlayerTimer, moveThirdPlayerTimer, thirdPlayerNameTimer);
 
-        const secondPlayerTimer = setTimeout(() => {
-            setSecondPlayerIsShown(true)
-        }, 5000);
-        const secondPlayerNameTimer = setTimeout(() => {
-            setSecondPlayerNameIsShown(true)
-        }, 7000);
-        const moveSecondPlayerTimer = setTimeout(() => {
-            setMoveSecondPlayer(true)
-        }, 9000);
+            const secondPlayerTimer = setTimeout(() => {
+                setSecondPlayerIsShown(true)
+            }, 5000);
+            const secondPlayerNameTimer = setTimeout(() => {
+                setSecondPlayerNameIsShown(true)
+            }, 7000);
+            const moveSecondPlayerTimer = setTimeout(() => {
+                setMoveSecondPlayer(true)
+            }, 9000);
 
-        const firstPlayerTimer = setTimeout(() => {
-            setFirstPlayerIsShown(true)
-        }, 9000);
-        const firstPlayerNameTimer = setTimeout(() => {
-            setFirstPlayerNameIsShown(true)
-        }, 11000);
-        const moveFirstPlayerTimer = setTimeout(() => {
-            setMoveFirstPlayer(true)
-        }, 13000);
-        timers.push(firstPlayerTimer, moveFirstPlayerTimer, firstPlayerNameTimer);
+            const firstPlayerTimer = setTimeout(() => {
+                setFirstPlayerIsShown(true)
+            }, 9000);
+            const firstPlayerNameTimer = setTimeout(() => {
+                setFirstPlayerNameIsShown(true)
+            }, 11000);
+            const moveFirstPlayerTimer = setTimeout(() => {
+                setMoveFirstPlayer(true)
+            }, 13000);
+            timers.push(firstPlayerTimer, moveFirstPlayerTimer, firstPlayerNameTimer);
 
-        const darkBackground = setTimeout(() => {
-            setDarkBackground(true)
-        }, 10000);
-        const light = setTimeout(() => {
-            setSpotLight(true);
-        }, 13000);
-        const removeDarkBackground = setTimeout(() => {
-            setDarkBackground(false);
-        }, 14000);
-        timers.push(secondPlayerTimer, moveSecondPlayerTimer, secondPlayerNameTimer, light);
+            const darkBackground = setTimeout(() => {
+                setDarkBackground(true)
+            }, 10000);
+            const light = setTimeout(() => {
+                setSpotLight(true);
 
-        const confettiTimer = setTimeout(() => {
-            setIsExploding(true);
-        }, 11000);
+            }, 10500);
+            const removeDarkBackground = setTimeout(() => {
+                setDarkBackground(false);
+            }, 14000);
+            timers.push(secondPlayerTimer, moveSecondPlayerTimer, secondPlayerNameTimer, light, removeDarkBackground, darkBackground);
 
-        const cheersPlay = setTimeout(() => {
-            trumpetsSound.play();
-            cheersSound.play();
-        }, 10700)
+            const confettiTimer = setTimeout(() => {
+                setIsExploding(true);
+            }, 11000);
 
-        timers.push(cheersPlay, confettiTimer);
+            const cheersPlay = setTimeout(() => {
+                trumpetsSound.play();
+                cheersSound.play();
+            }, 10700)
 
-        return () => {
-            timers.forEach(timer => clearTimeout(timer));
-            setSpotLight(false)
+            timers.push(cheersPlay, confettiTimer);
+
+            return () => {
+                timers.forEach(timer => clearTimeout(timer));
+                setSpotLight(false)
+            }
         }
-    }, []);
+    }, [step]);
+
+    console.log(step)
 
     return (
-        <div className={styles.newRanking_container}>
-            {isExploding &&
-                <ConfettiExplosion
-                    className={`confetti`}
-                    height={"100vh"}
-                    width={2000}
-                />}
-            <div
-                className={`${styles.thirdPlayer} ${thirdPlayerIsShown ? styles.appear : ''} ${moveThirdPlayer ? styles.move : ''}`}>
-                <span>3</span>
-                <div className={`${styles.playerName} ${thirdPlayerNameIsShown ? styles.playerNameIsShown : ''}`}>
-                    Mymy
-                </div>
-            </div>
-            <div
-                className={`${styles.secondPlayer} ${secondPlayerIsShown ? styles.appear : ''} ${moveSecondPlayer ? styles.move : ''}`}>
-                <span>2</span>
-                <div className={`${styles.playerName} ${secondPlayerNameIsShown ? styles.playerNameIsShown : ''}`}>
-                    Leila
-                </div>
-            </div>
-            <div
-                className={`${styles.firstPlayer} ${firstPlayerIsShown ? styles.appear : ''} ${moveFirstPlayer ? styles.move : ''}`}>
-                <span>1</span>
-                <div className={`${styles.playerName} ${firstPlayerNameIsShown ? styles.playerNameIsShown : ''}`}>
-                    {playersPoints.length > 0 && (
-                        <div className={styles.player_info}>
-                            {playersPoints[0].player.picture ?
-                                <img src={playersPoints[0].player?.picture}
-                                     alt={`avatar de ${playersPoints[0].player.name}`}/>
-                                :
-                                <img src={defaultAvatar} alt="user picture"/>
-                            }
-                            <h1>{playersPoints[0].player?.name}</h1>
+        <div>
+            {step === 1 && (<div>
+                <button style={{marginTop: "100px"}} onClick={() => setStep(prevState => prevState + 1)}>Podium</button>
+            </div>)
+            }
+            {step === 2 && (
+                <div className={styles.newRanking_container}>
+                    {isExploding &&
+                        <ConfettiExplosion
+                            className={`confetti`}
+                            height={"100vh"}
+                            width={2000}
+                        />}
+                    <div
+                        className={`${styles.thirdPlayer} ${thirdPlayerIsShown ? styles.appear : ''} ${moveThirdPlayer ? styles.move : ''}`}>
+                        <img src={thirdMedal} alt={"medal of third best player"} className={styles.podiumMedal}/>
+                        {playersPoints.length > 0 && <h1 className={`${styles.playerNameTitle} ${thirdPlayerNameIsShown ? styles.playerNameTitleIsShown : ''}`}>{playersPoints[2].player?.name}</h1>
+                        }
+                        <div
+                            className={`${styles.playerName} ${thirdPlayerNameIsShown ? styles.playerNameIsShown : ''}`}>
+                            {playersPoints.length > 0 && (
+                                <div className={styles.player_info}>
+                                    {playersPoints[2].player.picture ?
+                                        <img src={playersPoints[2].player?.picture}
+                                             alt={`avatar de ${playersPoints[2].player.name}`}/>
+                                        :
+                                        <img src={defaultAvatar} alt="user picture"/>
+                                    }
+                                </div>
+                            )}
                         </div>
-                    )}
+                    </div>
+                    <div
+                        className={`${styles.secondPlayer} ${secondPlayerIsShown ? styles.appear : ''} ${moveSecondPlayer ? styles.move : ''}`}>
+                        <img src={secondMedal} alt={"medal of second best player"} className={styles.podiumMedal}/>
+                        {playersPoints.length > 0 && <h1 className={`${styles.playerNameTitle} ${secondPlayerNameIsShown ? styles.playerNameTitleIsShown : ''}`}>{playersPoints[1].player?.name}</h1>
+                        }
+                        <div
+                            className={`${styles.playerName} ${secondPlayerNameIsShown ? styles.playerNameIsShown : ''}`}>
+                            {playersPoints.length > 0 && (
+                                <div className={styles.player_info}>
+                                    {playersPoints[1].player.picture ?
+                                        <img src={playersPoints[1].player?.picture}
+                                             alt={`avatar de ${playersPoints[1].player.name}`}/>
+                                        :
+                                        <img src={defaultAvatar} alt="user picture"/>
+                                    }
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                    <div
+                        className={`${styles.firstPlayer} ${firstPlayerIsShown ? styles.appear : ''} ${moveFirstPlayer ? styles.move : ''} `}>
+                        <img src={cup} alt={"cup of the Catan king"} className={styles.podiumMedal}/>
+                        {playersPoints.length > 0 && <h1 className={`${styles.playerNameTitle} ${firstPlayerNameIsShown ? styles.playerNameTitleIsShown : ''}`}>{playersPoints[0].player?.name}</h1>
+                        }
+                        <div
+                            className={`${styles.playerName} ${firstPlayerNameIsShown ? styles.playerNameIsShown : ''}`}>
+                            {playersPoints.length > 0 && (
+                                <div className={styles.player_info}>
+                                    {playersPoints[0].player.picture ?
+                                        <img src={playersPoints[0].player?.picture}
+                                             alt={`avatar de ${playersPoints[0].player.name}`}/>
+                                        :
+                                        <img src={defaultAvatar} alt="user picture"/>
+                                    }
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                    {spotLight ? (
+                        <div className={`${styles.lightCircleOff} ${darkBackground ? styles.lightCircleOn : ''}`}>
+                        </div>) : null}
+
                 </div>
-            </div>
-            <div className={`${styles.lightCircleOff} ${darkBackground ? styles.lightCircleOn : ''}`}>
-            </div>
+            )}
         </div>
     )
 }
