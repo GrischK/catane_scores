47954bd571ec54865284e2a6f964a7c236eb14d5
Grischka GORSKI <ggorski@amiltone.com>
2024-02-06 14:13:19 +0100

    🚧 WIP Accordion

--- client/src/components/Accordion.tsx
+++ client/src/components/Accordion.tsx
@@ -1,20 +1,27 @@
+import React from "react";
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
-import {PlayerData} from "../gql/generated/schema";
-import styles from "../screens/GamesList/GamesList.module.css";
-import defaultAvatar from "../assets/images/default_avatar.png";
-import React from "react";
+import {Score, User} from "../gql/generated/schema";
 
-interface GameAccordionProps {
-    winner: PlayerData;
-    participants: PlayerData[];
-    index: number;
+
+interface GameData {
+    id: number;
+    date?: string | null | undefined;
+    place?: string | null | undefined;
+    picture?: string | null | undefined;
+    players?: User[];
+    scores?: Score[] | null | undefined;
 }
 
-export default function GameAccordion({games, index}: GameAccordionProps) {
+interface GameAccordionProps {
+    game?: GameData;
+    index?: number;
+}
+
+export default function GameAccordion({game, index}: GameAccordionProps) {
     return (
         <div>
             <Accordion>
@@ -24,25 +31,25 @@ export default function GameAccordion({games, index}: GameAccordionProps) {
                     id="panel1-header"
                 >
                     <Typography>
-                        <div key={index}>
-                            <div>
-                                {score.player.picture ?
-                                    <img src={score.player.picture}
-                                         alt={`image de ${score.player.name}`}
-                                    />
-                                    :
-                                    <img src={defaultAvatar}
-                                         alt={`image de ${score.player.name}`}
-                                    />
-                                }
-                                <span className={styles.player_name}>
-                                                {score.player.name}
-                                            </span>
-                            </div>
-                            <span className={styles.player_score}>
-                                        {score.score}
-                                        </span>
-                        </div>
+                        {/*<div key={index}>*/}
+                        {/*    <div>*/}
+                        {/*        {score.player.picture ?*/}
+                        {/*            <img src={score.player.picture}*/}
+                        {/*                 alt={`image de ${score.player.name}`}*/}
+                        {/*            />*/}
+                        {/*            :*/}
+                        {/*            <img src={defaultAvatar}*/}
+                        {/*                 alt={`image de ${score.player.name}`}*/}
+                        {/*            />*/}
+                        {/*        }*/}
+                        {/*        <span className={styles.player_name}>*/}
+                        {/*                        {score.player.name}*/}
+                        {/*                    </span>*/}
+                        {/*    </div>*/}
+                        {/*    <span className={styles.player_score}>*/}
+                        {/*                {score.score}*/}
+                        {/*                </span>*/}
+                        {/*</div>*/}
                     </Typography>
                 </AccordionSummary>
                 <AccordionDetails>
---
+++ client/src/screens/Test/Test.tsx
@@ -0,0 +1,30 @@
+import GameAccordion from "../../components/Accordion";
+import {useGamesQuery} from "../../gql/generated/schema";
+
+export default function Test() {
+    const {data, refetch} = useGamesQuery()
+
+    const sortedGames = data?.games.slice().sort((a, b) => b.id - a.id);
+
+    const classedGames = sortedGames?.map((game) => {
+        if (game.scores) {
+            const sortedScores = [...game.scores];
+            sortedScores.sort((a, b) => b.score - a.score);
+            return {...game, scores: sortedScores};
+        } else {
+            return game;
+        }
+    });
+
+    console.log(classedGames)
+
+    return (
+        <>
+            {classedGames?.map((game, index) => (
+                    <GameAccordion game={game} key={index} index={index}/>
+                )
+            )
+            }
+        </>
+    )
+}
